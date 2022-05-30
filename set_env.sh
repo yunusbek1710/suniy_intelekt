ceeb3cc25447ae2a4b87165e568f706d6cd32fe7
iammatkarimov <matkarim1099@gmail.com>
2022-05-30 11:57:30 +0500

    compose file changes

--- set_env.sh
+++ set_env.sh
@@ -2,9 +2,9 @@
 
 export POSTGRES_HOST="localhost"
 export POSTGRES_PORT=5432
-export POSTGRES_USER="suniyintelekt"
+export POSTGRES_USER="district"
 export POSTGRES_PASSWORD="123"
-export POSTGRES_DATABASE="suniyintelekt"
+export POSTGRES_DATABASE="district"
 export POSTGRES_POOL_SIZE=1024
 export HTTP_HEADER_LOG=false
 export HTTP_BODY_LOG=false
